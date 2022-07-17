41fe2bcc8b30ca47185144d288ed9922296352db
vicky <vzhong2021@gmail.com>
2022-07-17 10:43:50 -0400

    Refactored into App.js and index.js and added config middleware

---
+++ app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express(); 
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('express-async-errors')
+
+const classRouter = require('./controllers/classes')
+const exchangeRouter = require('./controllers/exchanges')
+const usersRouter = require('./controllers/users')
+const loginRouter = require("./controllers/login")
+const middleware = require('./utils/middleware')
+
+mongoose.connect(config.MONGO_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/classes', classRouter)
+app.use('/api/exchanges', exchangeRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+
+app.use(middleware.errorHandler)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,41 +1,10 @@
-require("dotenv").config()
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-require('express-async-errors')
 
-const classRouter = require('./controllers/classes')
-const exchangeRouter = require('./controllers/exchanges')
-const usersRouter = require('./controllers/users')
+const server = http.createServer(app)
 
-const cors = require('cors')
-const loginRouter = require("./controllers/login")
-
-const app = express(); 
-
-app.use(express.json())
-app.use(cors())
-app.use(middleware.requestLogger)
-const mongoUrl = `mongodb+srv://vzhong2025:${process.env.PEPEPOPO}@cluster0.kcmgv.mongodb.net/classexchange?retryWrites=true&w=majority`
-mongoose.connect(mongoUrl)
-
-app.use('/api/classes', classRouter)
-app.use('/api/exchanges', exchangeRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
-
-const unknownEndpoint = (request, response) => {
-  console.log("brungus")
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-app.use(middleware.errorHandler)
-
-
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
